693f64ffa99cf5f3b1f2b7765de8ae7a36d97e15
SurajGupta620 <surajgupta6206@gmail.com>
2024-06-09 10:18:11 +0530

    Learning about operator in js

---
+++ 03_operator.js
@@ -0,0 +1,19 @@
+//Operator are used for perform operations
+
+let num1 = 10;
+let num2 = 5;
+
+//addition operation
+console.log("add : ", num1 + num2);
+
+//subtraction operation
+console.log("sub : ", num1 - num2);
+
+//multiplication operation
+console.log("mul : ", num1 * num2);
+
+//division operation 
+console.log("div : ", num1 / num2);
+
+//modulo operator - for finding remainder
+console.log("remainder : ", num1 % num2);
