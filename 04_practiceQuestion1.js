156eb936cbf9a07a2c4918c1de25d5cf80d5f156
SurajGupta620 <surajgupta6206@gmail.com>
2024-06-12 10:22:15 +0530

    practice some question in JS

---
+++ 04_practiceQuestion1.js
@@ -0,0 +1,18 @@
+//Ques- Declare your name as a string and print its length in JS.
+
+let name = "Suraj Gupta";
+console.log(name.length);
+
+
+
+//Ques- Declare your first name as a string and print its first character
+
+let firstName = "Suraj";
+console.log(firstName[0]);
+
+
+
+//Ques- Declare your first name as a string and print its last character
+
+let lastName = "Gupta";
+console.log(lastName[lastName.length-1]);
